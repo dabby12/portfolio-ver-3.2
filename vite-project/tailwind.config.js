c4da5408d6303ab542c3cedbc8e1b1fe8de28aee
Make_aguess <94730074+dabby12@users.noreply.github.com>
2024-12-14 18:15:23 +0800

    Update tailwind.config.js

--- vite-project/tailwind.config.js
+++ vite-project/tailwind.config.js
@@ -1,4 +1,5 @@
-module.exports = {
+/** @type {import('tailwindcss').Config} */
+export default {
   content: [
     './src/**/*.{html,js,jsx,ts,tsx}', // Make sure Tailwind knows about your JSX files
   ],
@@ -36,9 +37,9 @@ module.exports = {
         slideLeft: {
           '0%': { transform: 'translateX(100%)', opacity: 0 },
           '100%': { transform: 'translateX(0)', opacity: 1 },
+        },
       },
-      }
     },
   },
   plugins: [],
-}
+};
